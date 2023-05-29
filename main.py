f085ff8379df4bf97b9ee19c4df02dfbf4b4e369
Ritik Singh <56690279+TheRitikk@users.noreply.github.com>
2023-05-29 14:41:04 +0530

    Update main.py
    Second

--- main.py
+++ main.py
@@ -5,14 +5,20 @@ import webbrowser
 import openai
 from config import apikey
 import datetime
+import ecapture as ec
+import time
+import wikipedia
+import random
+import requests
+import subprocess
 
 chatstr = ""
 
 
-def chat(query):
+def chat(qs):
     global chatstr
     openai.api_key = apikey
-    chatstr += f"Ritik: {query}\n AI: "
+    chatstr += f"Ritik: {qs}\n AI: "
     response = openai.Completion.create(
         model="text-davinci-003",
         prompt=chatstr,
@@ -57,50 +63,108 @@ def say(text):
     engine.runAndWait()
 
 
-def takeCommand():
+def takecommand():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        r.pause_threshold = 1 #Waiting time to take command
+        r.adjust_for_ambient_noise(source, duration=1)
+        print("\nListening....")
+        r.pause_threshold = 1  # Waiting time to take command
         audio = r.listen(source)
         try:
             print("Recognizing......")
             query = r.recognize_google(audio, language="en-in")
             # print(f"User said : {query}")
             return query
+
         except Exception as e:
-            return "Some Error Occurred. Sorry Please try again!"
+            say("Some Error Occurred. Sorry Please try again!")
+            return "None"
 
 
 if __name__ == '__main__':
     print('Instruction : ')
     print("1. Need text file - using artificial intelligence")
     print("1. Reset chat - reset the cat")
-    print("2. Close - close this program\n\n")
+    print("2. Close - close this program\n")
     say("Hello Sir, How can i help you?")
     while True:
-        print("\nListening....")
-        query = takeCommand()
+        query = takecommand()
         # todo : add more sites
         sites = [["youtube", "https://youtube.com"], ["wikipedia", "https://wikipedia.com"],
                  ["google", "https://google.com"]]
         for site in sites:
             if f"Open {site[0]}".lower() in query.lower():
-                say(f"Opening {site[0]}e.....")
+                say(f"Opening {site[0]}.....")
                 webbrowser.open(site[1])
 
-        if "the time" in query:
+        if "time" in query:
             strfTime = datetime.datetime.now().strftime("%H:%M:%S")
             say(f"The time is {strfTime}")
 
         elif "using artificial intelligence".lower() in query.lower():
             ai(prompt=query)
 
+        elif "news".lower() in query.lower():
+            news = webbrowser.open_new_tab("https://timesofindia.indiatimes.com/home/headlines")
+            say('Here are some headlines from the Times of India,Happy reading')
+            time.sleep(6)
+
+        elif "camera" in query.lower() or "take a photo" in query.lower() or "take picture" in query.lower() or "take a picture" in query.lower():
+            ec.capture(0, "frame", f"Openai/{random.randint(0,99)}img.png")
+
+        elif "weather".lower() in query.lower():
+            api_key = "Enter your api kay"
+            base_url = "http://api.weatherapi.com/v1"
+            say("what is the city name")
+            city_name = takecommand()
+            complete_url = base_url + "/current.json?key=" + api_key + "&q=" + city_name
+            response = requests.get(complete_url)
+            x = response.json()
+            a = x.keys()
+            a = list(a)
+            if a[0] != "error":
+                y = x["current"]
+                current_temperature = y["temp_c"]
+                current_humidiy = y["humidity"]
+                z = x["current"]
+                weather_description = z["condition"]["text"]
+                say(" Temperature is " +
+                      str(current_temperature) +"degree celcius" +
+                      "\n humidity is " +
+                      str(current_humidiy) +"percentage" +
+                      "\n description  " +
+                      str(weather_description))
+            
+            else:
+                say("Some error accured.\n Please check my code and api kay,\n try again?")
+        
+        elif 'wikipedia' in query.lower():
+            say('Searching Wikipedia...')
+            statement =query.replace("wikipedia", "")
+            results = wikipedia.summary(statement)
+            say("According to Wikipedia")
+            print(results)
+            say(results)
+            
         elif "Close".lower() in query.lower():
             quit()
 
-        elif "reset chat".lower() in query.lower():
-            chat += ""
+        elif "clear chat" in query.lower() or "reset chat" in query.lower():
+            chatstr += ""
+            say("Chat has been reset.")
+            
+        elif "log off" in query.lower() or "sign out" in query.lower():
+            say("Ok , your pc will log off after 10 second, make sure you exit from all applications")
+            subprocess.call(["shutdown", "/l"])
+            
+        elif "shut down my pc" in query.lower():
+            say("Ok , your pc will shut down after 10 second, make sure you exit from all applications")
+            subprocess.call(["shutdown", "/s"])
+            
+        elif "restart my pc" in query.lower():
+            say("Ok , your pc will restart after 10 second, make sure you exit from all applications")
+            subprocess.call(["shutdown", "/r"])
 
         else:
             print("Chating.....")
-            chat(query)
+            chat(qs=query)
